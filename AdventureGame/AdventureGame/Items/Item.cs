bde5b00003c483642b8a0711fd3e7f5a91ff88a5
DommiHD <DommiHD@gmail.com>
2017-05-25 21:08:16 +0200

    .

--- AdventureGame/AdventureGame/Items/Item.cs
+++ AdventureGame/AdventureGame/Items/Item.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using AdventureGame.Handler;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AdventureGame.Items
 {
     class Item
     {
+        private string n;
+        private string tp;
+        private PictureBox p;
+
+        public Item(string name,string texturepath, PictureBox pbox)
+        {
+            n = name;
+            tp = texturepath;
+            if (pbox == null)
+            {
+                p = new PictureBox();
+            }
+            else
+            {
+                p = pbox;
+            }
+            if (texturepath!=null)
+            {
+                p.Image = Image.FromFile(texturepath);
+            }
+            ItemHandler.addItem(this);
+
+        }
+
+
+        public void setTexturepath(string texturepath)
+        {
+            tp = texturepath;
+        }
+
+        public string getTexturepath()
+        {
+            return tp;
+        }
+        public void setName(string name)
+        {
+            n = name;
+        }
+
+        public string getName()
+        {
+            return n;
+        }
+
+        public void spawn(int x, int y)
+        {
+
+        }
+
+        public void despawn()
+        {
+            p.Hide();
+        }
     }
 }
